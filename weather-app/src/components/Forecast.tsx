b3836fe94749f9ce8a7fc619163265da7169d3a5
tittieth <114925706+tittieth@users.noreply.github.com>
2023-07-25 22:19:48 +0200

    show weather

--- weather-app/src/components/Forecast.tsx
+++ weather-app/src/components/Forecast.tsx
@@ -4,13 +4,50 @@ type Props = {
     data: forecastType
 }
 
+const Degree = ({ temp }: {temp: number}): JSX.Element => (
+    <span>
+        {temp}<sup>o</sup>
+    </span>
+)
+
 const Forecast = ({ data }: Props): JSX.Element => {
     console.log(data);
+    const today = data.list[0]
     
     return ( 
-    <div>
-        <p>{data.name}</p>
-    </div> );
+   <div className="w-full md:max-w-[500px] py-4 md:py4 md:px-10 lg:px24 h-full lg:h-auto bg-white
+   bg-opacity-20 backdrop-blur-ls rounded drop-shadow-lg">
+    <div className="mx-auto w-[300px]">
+        <section className="text-center">
+            <h2 className="text-2xl font-black">
+                {data.name}
+                <span className="font-thin"> {data.country}</span>
+            </h2>
+            <h1 className="text-4xl font-extrabold">
+                <Degree temp={Math.round(today.main.temp)} />
+            </h1>
+            <p className="text-sm">
+                {today.weather[0].main}{' '}
+                {today.weather[0].description}
+            </p>
+            <p className="text-sm">
+                H: <Degree temp={Math.ceil(today.main.temp_max)} /> {' '}
+                L: <Degree temp={Math.floor(today.main.temp_min)} />
+            </p>
+        </section>
+
+        <section className="flex overflow-x-scroll mt-4 pb-2 mb-5">
+            {data.list.map((item, i) => (
+                <div className="inline-block text-center w-[50px] flex-shrink-0" key={i}>
+                    <p className="text-sm">{i === 0 ? 'Now' : new Date(item.dt * 1000).getHours()}</p>
+                    <img alt={`weather-icon-${item.weather[0].description}`} 
+                    src={`http://openweathermap.org/img/wn/${item.weather[0].icon}@2x.png`}/>
+                    <p><Degree temp={Math.round(item.main.temp)} /></p>
+                </div>
+            ))}
+        </section>
+    </div>
+   </div> );
 }
 
 export default Forecast;
